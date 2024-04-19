680a5efa3c79631c257de193ac48c922980d4d98
Amit Kumar Gupta <113920422+Amit-IITM@users.noreply.github.com>
2024-04-19 22:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ def addition(a,b):
 a = st.number_input("first number : ")
 b = st.number_input("second number : ")
 
-st.text("here is the sum",addition(a,b))
+st.text(addition(a,b))
 st.text("dedicated to dalyodh Paji")
